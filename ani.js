84038de7aebbff0c36cb0a07a717fd9b93da8f75
MrDoge225 <sonofmydad@live.com>
2019-07-09 14:33:36 +0200

    Next stages almost there
    Co-authored-by: Jan Dolejší <honza.dolejsi@gmail.com>

--- ani.js
+++ ani.js
@@ -4,7 +4,7 @@ function showState(stateId) {
 
 function showStateJson(stateJson) {
     console.log(stateJson);
-    var onTablePredicates = stateJson.predicates.filter(p => p.name==="ontable");
+    var onTablePredicates = stateJson.predicates.filter(p => p.name === "ontable");
     console.log(onTablePredicates);
 
     var towerBottomBlocks = onTablePredicates.map(p => p.args[0]);
@@ -16,20 +16,50 @@ function showStateJson(stateJson) {
     towers.forEach(t => buildUp(t, stateJson));
 
     var canvas = document.getElementById("canvas");
-    //todo: for each tower.. and each block, call createBlock
+    while (canvas.firstChild) {
+        canvas.removeChild(canvas.firstChild);
+    }
+    var gripElement = createGripper();
+    canvas.appendChild(gripElement)
+    // for each tower.. and each block, call createBlock and add as a child to 'canvas'
+    for (let towerIndex = 0; towerIndex < towers.length; towerIndex++) {
+        const tower = towers[towerIndex];
+        console.log(tower)
+
+        for (let level = 0; level < tower.length; level++) {
+            const blockColor = tower[level];
+            console.log(`Tower: ${towerIndex}, Block: ${level}, ${blockColor}`);
+            var blockElement = createBlock(blockColor, towerIndex, level);
+            canvas.appendChild(blockElement);
+        }
+    }
+    // var onTablePredicates = stateJson.predicates.filter(p => p.name === "ontable");
+    // console.log(onTablePredicates);
 }
-function createBlock(id, tower, level){
+
+function createBlock(color, tower, level) {
+    // <div class="block" style="left:0px; bottom:0px; background-color:red" />
     var element = document.createElement("div");
     element.className = "block";
-    element.style="bottom: "+(50*level+1)+"px; left: "+(50*tower+1)+"px; background-color: "+id;
+    element.style = "bottom: " + (50 * level + 1) + "px; left: " + (50 * tower + 1) + "px; background-color: " + color;
     element.setAttribute("tower", tower);
     element.setAttribute("level", level);
     return element;
 }
+
+function createGripper() {
+    // <img class="gripper" src="gripper-opened-implicit.svg">
+    var element = document.createElement("img");
+    element.className = "gripper";
+    element.style = "top:0px; left:0px;"
+    element.setAttribute("src", "gripper-opened-implicit.svg");
+    return element;
+}
+
 function buildUp(t, stateJson) {
-    var onPredicates = stateJson.predicates.filter(p => p.name==="on");
+    var onPredicates = stateJson.predicates.filter(p => p.name === "on");
     var topBlock = t[0];
-    while(topBlock) {
+    while (topBlock) {
         var predicate1 = onPredicates.find(p => p.args[1] === topBlock);
         if (predicate1) {
             var onTopBlock = predicate1.args[0];
@@ -44,24 +74,23 @@ function buildUp(t, stateJson) {
 }
 
 function loadJSON(filePath, success, error) {
-    
-	var xhr = new XMLHttpRequest();
-	xhr.onreadystatechange = function()
-	{
-		if (xhr.readyState === XMLHttpRequest.DONE) {
-			if (xhr.status === 200) {
-				if (success)
-					success(JSON.parse(xhr.responseText));
-	} else {
-			if (error)
-				error(xhr);
-			}
-		}
-	};
-	xhr.open("GET", filePath, true);
-	xhr.send();
+
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                if (success)
+                    success(JSON.parse(xhr.responseText));
+            } else {
+                if (error)
+                    error(xhr);
+            }
+        }
+    };
+    xhr.open("GET", filePath, true);
+    xhr.send();
 }
 
-function onError(error){
+function onError(error) {
     console.error(error);
 }
